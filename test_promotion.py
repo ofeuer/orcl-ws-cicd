93fa139fb66303bd4072327ec3279c8d5d9fefd0
The Oracle Software Owner User <oracle@olif-vm.sub08091537440.olifvcn.oraclevcn.com>
2020-08-10 15:49:42 +0000

    Fix unit tests with correct data types and numeric decision

--- test_promotion.py
+++ test_promotion.py
@@ -14,7 +14,7 @@ class TestPromotion:
         assert '1200' == promotion.addition(1150, 50)
 
     def test_increment(self):
-        assert '1250' == promotion.increment(1000, 25)
+        assert '1250.0' == promotion.increment(1000, 25)
 
     def test_decrease(self):
         assert '970' == promotion.decrease(1150, 180)
